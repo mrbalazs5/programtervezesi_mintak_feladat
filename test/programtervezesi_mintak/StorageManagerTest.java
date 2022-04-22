4e1105b94c27e0bea5a435e1b621cd09ab12d46e
mrbalazs5 <megbalaa@gmail.com>
2022-04-22 13:35:59 +0200

    singleton test

---
+++ test/programtervezesi_mintak/StorageManagerTest.java
@@ -0,0 +1,19 @@
+package programtervezesi_mintak;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.sameInstance;
+
+import org.junit.jupiter.api.Test;
+
+class StorageManagerTest {
+
+	@Test
+	void testGetInstance() {
+		StorageManager storageManager1 = StorageManager.getInstance();
+		StorageManager storageManager2 = StorageManager.getInstance();
+
+		assertThat(storageManager1, sameInstance(storageManager2));
+	}
+
+}
